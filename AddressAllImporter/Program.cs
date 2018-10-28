e6144d049684c00e7ab9845fd600bdd119f34c48
Kurihara Jun <lh182051+src@gmail.com>
2018-10-29 00:08:24 +0900

    プロジェクト ファイルを追加します。

---
+++ AddressAllImporter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressAllImporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
